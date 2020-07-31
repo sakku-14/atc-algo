af1752c4994a60fe388959f9594cabc450151962
yuki sakuma <kumasan.04.24.14@gmail.com>
2020-07-31 17:15:27 +0900

    F問題文字列の扱いについて確認

---
+++ algo1-f*.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void){
+    int i=0, counter = 0;
+    char line[100000];
+    scanf("%s", line);
+
+    while(line[i] != '\0'){
+        if(line[i] >= 'A' && line[i] <= 'Z'){
+
+        }
+        i++;
+    }
+    i=0;
+
+    return 0;
+}
